c684328bba983a1905703ec354d0fee2908a0c4f
rawyurs <135272697+rawyurs@users.noreply.github.com>
2024-01-17 18:12:22 +0000

    primeiro commit

--- src/components/Alojamentos.jsx
+++ src/components/Alojamentos.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import Slider from "react-slick";
+//ola
 import { MdKingBed } from "react-icons/md";
 import { MdBathtub } from "react-icons/md";
 import { FaWifi } from "react-icons/fa";
